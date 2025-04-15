7ea9da5ab2aabdac2d9ab7802a0a95e2c117f483
sarahmds <sarah.santos@escolar.ifrn.edu.br>
2025-04-15 14:43:11 +0000

    exercicios

---
+++ listas/lista1.py
@@ -0,0 +1,2 @@
+print("Olá, Gilbert")
+print("Bem-vindo(a) ao Codespaces, Gilbert")
---
+++ listas/lista2_ex1.py
@@ -0,0 +1,5 @@
+#1
+print("Digite seu nome completo: ")
+nome = input()
+print("bem vindo(A) ao python,", nome.split()[0])
+
