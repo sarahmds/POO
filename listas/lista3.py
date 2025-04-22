45b1550fd4d9e43229a0667e548a49a1f27e6430
sarahmds <sarah.santos@escolar.ifrn.edu.br>
2025-04-22 14:45:56 +0000

    correção2

--- listas/lista3.py
+++ listas/lista3.py
@@ -15,10 +15,9 @@ volume = (4/3) * 3.14159 * (raio**3)
 print(f"VOLUME = {volume:.3f}")
 
 # Beecrowd 2416 – Corrida
-n1, n2 = map(int, input().split())
-tempo = int(input())
-velocidade = (n1 + n2) / tempo
-print(f"{velocidade:.2f}")
+C, N = map(int, input().split())
+ponto_termino = C % N
+print(ponto_termino)
 
 # Beecrowd 1015 – Distância Entre Dois Pontos
 x1, y1 = map(float, input().split())
@@ -27,9 +26,7 @@ distancia = ((x2 - x1)**2 + (y2 - y1)**2) ** 0.5
 print(f"{distancia:.4f}")
 
 # Beecrowd 1930 – Tomadas
-n = int(input())
-tomadas_totais = 0
-for _ in range(n):
-    tomadas = int(input())
-    tomadas_totais += tomadas - 1
+T1, T2, T3, T4 = map(int, input().split())
+tomadas_totais = T1 + T2 + T3 + T4
+tomadas_totais -= 3
 print(tomadas_totais)
