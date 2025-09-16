7b9a59a2f06df07cb7853faa024e534f4866821f
sarahmds <sarah.santos@escolar.ifrn.edu.br>
2025-09-16 16:46:08 +0000

    cadastro de serviço adicionado

--- Agenda/index.py
+++ Agenda/index.py
@@ -1,7 +1,15 @@
+import streamlit as st
 from templates.manterclienteUI import ManterClienteUI
+from templates.manterservicoUI import ManterServicoUI
 
 class IndexUI:
     def main():
-        ManterClienteUI.main()
+        st.sidebar.title("Menu")
+        opcao = st.sidebar.selectbox("Escolha uma opção", ["Clientes", "Serviços"])
+
+        if opcao == "Clientes":
+            ManterClienteUI.main()
+        elif opcao == "Serviços":
+            ManterServicoUI.main()
 
 IndexUI.main()
--- Agenda/views.py
+++ Agenda/views.py
@@ -1,7 +1,9 @@
 from models.cliente import Cliente, ClienteDAO
+from models.servico import Servico, ServicoDAO  
 
 class View:
 
+    # ---- CLIENTES ----
     def cliente_inserir(nome, email, fone):
         cliente = Cliente(0, nome, email, fone)
         ClienteDAO.inserir(cliente)
@@ -19,3 +21,19 @@ class View:
     def cliente_excluir(id):
         cliente = Cliente(id, "", "", "")
         ClienteDAO.excluir(cliente)    
+
+    # ---- SERVIÇOS ----
+    def servico_listar():
+        return ServicoDAO.listar()
+
+    def servico_inserir(descricao, preco):
+        servico = Servico(0, descricao, preco)
+        ServicoDAO.inserir(servico)
+
+    def servico_atualizar(id, descricao, preco):
+        servico = Servico(id, descricao, preco)
+        ServicoDAO.atualizar(servico)
+
+    def servico_excluir(id):
+        servico = Servico(id, "", 0)
+        ServicoDAO.excluir(servico)
