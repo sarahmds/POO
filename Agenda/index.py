eaca084c1dfb7ff424dda7a5db36fe5fa2cbe0e1
sarahmds <sarah.santos@escolar.ifrn.edu.br>
2025-09-18 14:06:51 +0000

    index alteração

--- Agenda/index.py
+++ Agenda/index.py
@@ -1,15 +1,18 @@
-import streamlit as st
 from templates.manterclienteUI import ManterClienteUI
 from templates.manterservicoUI import ManterServicoUI
+import streamlit as st
 
 class IndexUI:
-    def main():
-        st.sidebar.title("Menu")
-        opcao = st.sidebar.selectbox("Escolha uma opção", ["Clientes", "Serviços"])
 
-        if opcao == "Clientes":
-            ManterClienteUI.main()
-        elif opcao == "Serviços":
-            ManterServicoUI.main()
+    def menu_admin():            
+        op = st.sidebar.selectbox("Menu", ["Cadastro de Clientes", "Cadastro de Serviços"])
+        if op == "Cadastro de Clientes": ManterClienteUI.main()
+        if op == "Cadastro de Serviços": ManterServicoUI.main()
+
+    def sidebar():
+        IndexUI.menu_admin()
+
+    def main():
+        IndexUI.sidebar()
 
 IndexUI.main()
